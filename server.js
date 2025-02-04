b2db933a84fda1c025170ddd7f8b18e58d3a051c
popic112 <popic_popic@yahoo.com>
2025-02-04 17:08:27 +0200

    updates

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.use("/api/qrcodes", qrRoutes);
 
 // Pornim serverul
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
