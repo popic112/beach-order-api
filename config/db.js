b574ba90f6168bbe0651f51b472822e58c686ab1
popic112 <popic_popic@yahoo.com>
2025-02-05 20:36:53 +0200

    mysql2 in loc de mariadb

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
-const mariadb = require('mariadb');
-require("dotenv").config();
+const mysql = require("mysql2/promise");
+require("dotenv").config({ path: "./.env" });
 
-const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 5
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = pool;
