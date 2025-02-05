f6e3fd92302882be341887192172fd1b90c89fc9
popic112 <popic_popic@yahoo.com>
2025-02-05 21:53:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/qrcodes", qrRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
